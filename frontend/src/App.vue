6cb7d93cd7f0ffcda0e2214f280b70f5cbc9c1fe
thomassales <salesthomas96@gmail.com>
2024-01-11 14:17:13 +0100

    part3

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,37 +1,40 @@
 <template>
   <div class="max-w-md mx-auto mt-8">
-     <!-- Email Input -->
-     <label class="block mt-4 text-sm font-medium text-gray-700">Email</label>
-     <input v-model="formData.email" type="email" class="mt-1 p-2 border rounded-md w-full" />
-     <div v-if="!isEmailValid" class="text-red-500 text-sm mt-1">Entrez une adresse mail valide</div>
+      <!-- Email Input -->
+      <label class="block mt-4 text-sm font-medium text-gray-700">Email</label>
+      <input v-model="formData.email" type="email" class="mt-1 p-2 border rounded-md w-full" />
+      <div v-if="!isEmailValid" class="text-red-500 text-sm mt-1">Entrez une adresse mail valide</div>
 
-     <!-- Category Level Select -->
-     <label class="block mt-4 text-sm font-medium text-gray-700">Catégorie</label>
-     <select v-model="formData.category" class="mt-1 p-2 border rounded-md w-full">
-        <option value="furniture">Mobilier</option>
-        <option value="computer">Informatique</option>
-        <option value="other">Autre</option>
-     </select>
-     <div v-if="!isCategoryValid" class="text-red-500 text-sm mt-1">Choisissez la catégorie</div>
+      <!-- Category Level Select -->
+      <label class="block mt-4 text-sm font-medium text-gray-700">Catégorie</label>
+      <select v-model="formData.category" class="mt-1 p-2 border rounded-md w-full">
+          <option value="furniture">Mobilier</option>
+          <option value="computer">Informatique</option>
+          <option value="other">Autre</option>
+      </select>
+      <div v-if="!isCategoryValid" class="text-red-500 text-sm mt-1">Choisissez la catégorie</div>
 
-     <!-- Description Textarea -->
-     <label class="block mt-4 text-sm font-medium text-gray-700">Description</label>
-     <textarea v-model="formData.description" class="mt-1 p-2 border rounded-md w-full"></textarea>
-     <div v-if="!isDescriptionValid" class="text-red-500 text-sm mt-1">La description ne doit pas être vide</div>
+      <!-- Description Textarea -->
+      <label class="block mt-4 text-sm font-medium text-gray-700">Description</label>
+      <textarea v-model="formData.description" class="mt-1 p-2 border rounded-md w-full"></textarea>
+      <div v-if="!isDescriptionValid" class="text-red-500 text-sm mt-1">La description ne doit pas être vide</div>
 
-     <!-- Priority Select -->
-     <label class="block mt-4 text-sm font-medium text-gray-700">Priorité</label>
-     <select v-model="formData.priority" class="mt-1 p-2 border rounded-md w-full">
-        <option value="low">Faible</option>
-        <option value="normal">Normale</option>
-        <option value="high">Haute</option>
-     </select>
-     <div v-if="!isPriorityValid" class="text-red-500 text-sm mt-1">Choisissez la priorité</div>
+      <!-- Priority Select -->
+      <label class="block mt-4 text-sm font-medium text-gray-700">Priorité</label>
+      <select v-model="formData.priority" class="mt-1 p-2 border rounded-md w-full">
+          <option value="low">Faible</option>
+          <option value="normal">Normale</option>
+          <option value="high">Haute</option>
+      </select>
+      <div v-if="!isPriorityValid" class="text-red-500 text-sm mt-1">Choisissez la priorité</div>
 
-     <!-- Submit Button -->
-     <button @click="submitForm" :disabled="!isFormValid" class="mt-4 p-2 text-white rounded-md hover:valid:bg-blue-600 bg-blue-500 disabled:bg-red-600">
+      <!-- Submit Button -->
+      <button @click="submitForm" :disabled="!isFormValid" class="mt-4 p-2 text-white rounded-md hover:valid:bg-blue-600 bg-blue-500 disabled:bg-red-600">
         Valider
-     </button>
+      </button>
+
+
+      <router-view></router-view>
   </div>
 </template>
 
@@ -45,9 +48,17 @@ const formData = ref({
    priority: null,
 })
 
-const submitForm = () => {
-   console.log('Form submitted with data:', formData.value)
+const submitForm = async () => {
+  console.log('Form submitted with data:', formData.value)
+  const response = await fetch('/api/ticket', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(formData.value),
+  });
 }
+
 
 const isEmailValid = computed(() => {
    if (!formData.value) return false
@@ -59,6 +70,8 @@ const isDescriptionValid = computed(() => formData.value.description && formData
 const isPriorityValid = computed(() => !!formData.value.priority)
 
 const isFormValid = computed(() => isEmailValid.value && isCategoryValid.value && isDescriptionValid.value && isPriorityValid.value)
+
+
 
 </script>
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ command, mode }) => {
+   return {
+      plugins: [
+         vue(),
+      ],
+      server: {
+         port: 5173,
+         open: true,
+         proxy: {
+            '^/api/.*': {
+               target: 'http://localhost:3000',
+               ws: true,
+               secure: false,
+               changeOrigin: true,
+            },
+         }
+      },
+   }
 })
