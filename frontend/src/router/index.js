3649f3d64aa8ca120d3b0753da557522e04e2c1b
thomassales <salesthomas96@gmail.com>
2024-01-12 18:51:49 +0100

    add Card & Detail componant

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
 import TicketForm from '../views/TicketForm.vue'
+// import Recap from '../views/Recap.vue'
 
 const routes = [
     {
@@ -8,6 +9,27 @@ const routes = [
         component: TicketForm,
 
     },
+    {
+        path: "/recap/:ticketId",
+        component: () => import('/src/views/Recap.vue'),
+        props: true,
+
+    },
+    {
+        path: "/tickets",
+        component: () => import('/src/views/Tickets.vue'),
+        children: [
+            {
+                path: ":ticketId",
+                component: () => import('/src/components/TicketDetail.vue'),
+                props: true,
+            }
+
+        ]
+
+    }
+
+
 ]
 const router = createRouter({
     history: createWebHistory('/'),
---
+++ frontend/src/views/Tickets.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="">
+       <div class=" flex">
+          <!-- list -->
+          <div class="" >
+             <template v-for="ticket in ticketList" class="p-4">
+                <TicketCard :ticketId="ticket.id" @click="onClick(ticket.id)" :selected="ticket.id == selectedTicketId"></TicketCard>
+             </template>
+          </div>
+ 
+          <!-- detail -->
+          <router-view class="w-full"></router-view>
+       </div>
+    </div>
+ </template>
+ 
+ <script setup>
+ import { ref, onMounted } from 'vue'
+ import { useRoute } from 'vue-router'
+ 
+ import TicketCard from '/src/components/TicketCard.vue'
+ import router from '/src/router'
+ 
+ const ticketList = ref([])
+ 
+ const route = useRoute()
+ const selectedTicketId = ref(route.params.ticketId)
+ 
+ 
+ onMounted(async () => {
+    const response = await fetch('/api/tickets')
+    ticketList.value = await response.json()
+ })
+ 
+ const onClick = (ticketId) => {
+    selectedTicketId.value = ticketId
+    router.push(`/tickets/${ticketId}`)
+ }
+ </script>
